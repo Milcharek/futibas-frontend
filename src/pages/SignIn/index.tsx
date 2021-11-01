079a705bc09b65d9f1d5a3b1ce4660176de0dfd1
Filipe Milcharek <ofilipedavila@gmail.com>
2021-11-01 09:59:38 -0300

    Add useAuth hook

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -14,12 +14,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles'
 
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const { signIn } = useContext(AuthContext);
+    const { signIn } = useAuth();
 
-    const handleSubmit = useCallback(async (data: object) => {
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         try {
             formRef.current?.setErrors({});
 
@@ -32,7 +37,10 @@ const SignIn: React.FC = () => {
                 abortEarly: false
             });
 
-            signIn();
+            signIn({
+                email: data.email,
+                password: data.password
+            });
         } catch (err) {
             const errors = getValidationErrors(err as Yup.ValidationError);
 
